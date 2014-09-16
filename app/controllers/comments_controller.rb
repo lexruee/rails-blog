37ab0321fb9e5ae5d151373a94b354b4f9a80083
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-09-17 00:22:00 +0200

    implement comments and fix stupid mistake in the tutorial!

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to articles_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to articles_path(@article)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # add resource articles
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
+
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
